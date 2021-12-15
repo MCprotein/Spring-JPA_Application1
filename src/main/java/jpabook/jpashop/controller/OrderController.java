09e0aa7676ad5fe0fa2dd4f35748c3ca1198114d
MCprotein <chss3339@gmail.com>
2021-12-15 20:27:37 +0900

    주문 목록 검색, 취소 _ 주문취소 404에러

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,16 +1,16 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderSerivce;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +41,20 @@ public class OrderController {
 
         orderSerivce.order(memberId, itemId, count);
         return "redirect:/orders";
+    }
+
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderSerivce.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList.html";
+
+    }
+
+    @PostMapping(value = "/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderSerivce.cancelOrder(orderId);
+        return "redirect:orders";
     }
 }
